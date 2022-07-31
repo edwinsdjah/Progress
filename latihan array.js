36aa1f1835dcd7d9c95ebd93fd27df6faa4542b4
edwinsdjah <53396367+edwinsdjah@users.noreply.github.com>
2022-07-31 09:41:17 +0700

    Latihan array

---
+++ latihan array.js
@@ -0,0 +1,75 @@
+// CONTOH LATIHAN ARRAY DENGAN MENGGUNAKAN KASUS NAIK ANGKOT
+
+// Deklarasi variable awal array
+let arrPenumpang = [];
+
+// Function utk menambahkan parameter ke dalam array
+function tambah(nama) {
+    // Jika isi array masih kosong, maka data langsung masuk
+    if (arrPenumpang.length === 0) {
+        arrPenumpang.push(nama)
+        return arrPenumpang
+    } else {
+        // Variabel kosong utk menampung isi value fungsi cari
+        let s = ""
+        // Fungsi utk mencari apakah nama penumpang sudah ada atau belum
+        function cari(nama) {
+            // Looping value array
+            for( i = 0 ; i < arrPenumpang.length ; i++){
+                // Jika value nama sama dgn isi array maka variabel s = true
+                if (nama === arrPenumpang[i]){
+                    return s = true
+                } 
+                // Jika value nama tidak ada di dalam array maka variabel s = false,
+                // looping hingga nilai i = isi array paling terakhir
+                else if (i === arrPenumpang.length - 1) { 
+                    return s = false
+                }
+            }
+        }
+        // Menjalankan fungsi nama
+        cari(nama);
+        // looping array untuk mengisi nilai
+        for (i = 0; i < arrPenumpang.length; i++) {
+            // Jika isi array ada yg undefined dan namanya belum ada di dalam array. langsung masuk ke array kosong tersebut
+            if (arrPenumpang[i] === undefined && s === false) {
+                arrPenumpang[i] = nama
+                return arrPenumpang
+            } 
+            // Jika namanya sudah ada, maka loop berakhir
+            else if (s === true) {
+                console.log(`${nama} sudah naik`)
+                return arrPenumpang
+            }
+            // Jika nama blm ada, dan array penuh semua, maka otomatis masuk ke paling belakang
+            else if (i === arrPenumpang.length - 1) {
+                arrPenumpang.push(nama)
+                return arrPenumpang
+            }
+        }
+    }
+}
+
+// Fungsi utk menghapus nama di dalam array
+function turun(nama) {
+    // Jika array masih kosong maka fungsi berakhir
+    if (arrPenumpang.length === 0) {
+        console.log(`Arraynya kosong`);
+    } else if (nama == undefined) {
+        console.log(`goblok`)
+    } else {
+        // looping array
+        for (i = 0; i < arrPenumpang.length; i++) {
+            // Jika menemukan nama yg sama dgn isi array maka langsung ubah menjadi undefined
+            if (arrPenumpang[i] === nama) {
+                arrPenumpang[i] = undefined;
+            } 
+            // Jika nama tidak ada, fungsi berakhir
+            // looping sampai nilai terakhir array
+            else if (i === arrPenumpang.length - 1) {
+                console.log(`${nama} tidak ada`);
+            }
+        }
+    }
+    return arrPenumpang;
+}
